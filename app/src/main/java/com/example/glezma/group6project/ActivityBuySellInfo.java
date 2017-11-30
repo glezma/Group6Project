d9b20f8349aca23526394d64a36c82d4307ae246
Avneesh <avneeshs@umich.edu>
2017-11-30 12:14:38 -0500

    Buy and sell information activity

---
+++ app/src/main/java/com/example/glezma/group6project/ActivityBuySellInfo.java
@@ -0,0 +1,15 @@
+package com.example.glezma.group6project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityBuySellInfo extends Activity {
+
+    private int a;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buy_sell_info);
+    }
+}
